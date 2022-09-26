f97b6b0ec07c5872a76143bd3264f9d62eb5da27
@pudgebooster <ukraniandollar@gmail.com>
2022-09-26 16:11:33 +0300

    feature: add newpermutationcipher func

--- permutation/permutation.go
+++ permutation/permutation.go
@@ -4,6 +4,10 @@ type PermutationCipher struct {
 	key []int
 }
 
+func NewPermutationCipher(key []int) *PermutationCipher {
+	return &PermutationCipher{key: key}
+}
+
 func (c PermutationCipher) encryptBlock(block []byte) []byte {
 	result := make([]byte, len(block))
 	for i := range block {
