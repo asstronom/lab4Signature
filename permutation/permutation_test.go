31a50a744729353d6918a7aca67fd218b856ee38
@pudgebooster <ukraniandollar@gmail.com>
2022-09-26 16:29:56 +0300

    test: complete test for encrypt block

--- permutation/permutation_test.go
+++ permutation/permutation_test.go
@@ -2,6 +2,15 @@ package permutation
 
 import "testing"
 
-func TestencryptBlock(t *testing.T) {
-	
+func TestEncryptBlock(t *testing.T) {
+	key := []int{4, 0, 2, 1, 3}
+	block := []byte{1, 2, 3, 4, 5}
+	correct := []byte{5, 1, 3, 2, 4}
+	cipher := NewPermutationCipher(key)
+	block = cipher.encryptBlock(block)
+	for i, v := range block {
+		if v != correct[i] {
+			t.Errorf("wrong encryption, %v != %v", block, correct)
+		}
+	}
 }
