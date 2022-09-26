0520fb1b9e829e61766fd8d4a57507a40448c680
@pudgebooster <ukraniandollar@gmail.com>
2022-09-26 16:09:58 +0300

    fix: move source code from _test file

--- permutation/permutation_test.go
+++ permutation/permutation_test.go
@@ -1,13 +1,3 @@
 package permutation
 
-type PermutationCipher struct {
-	key []int
-}
 
-func (c PermutationCipher) encryptBlock(block []byte) []byte {
-	result := make([]byte, len(block))
-	for i := range block {
-		result[i] = block[c.key[i]]
-	}
-	return result
-}
