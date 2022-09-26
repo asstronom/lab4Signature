2814b5ef708c379cc0a29245ea87bd14f92964be
@pudgebooster <ukraniandollar@gmail.com>
2022-09-26 16:44:56 +0300

    feature: add TestEncrypt

--- permutation/permutation_test.go
+++ permutation/permutation_test.go
@@ -14,3 +14,16 @@ func TestEncryptBlock(t *testing.T) {
 		}
 	}
 }
+
+func TestEncrypt(t *testing.T) {
+	key := []int{4, 0, 2, 1, 3}
+	message := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17}
+	correct := []byte{5, 1, 3, 1, 4, 10, 6, 8, 7, 9, 15, 11, 13, 12, 14, 16, 17}
+	cipher := NewPermutationCipher(key)
+	result := cipher.Encrypt(message)
+	for i, v := range result {
+		if v != correct[i] {
+			t.Errorf("wrong encryption, %v != %v", result, correct)
+		}
+	}
+}
