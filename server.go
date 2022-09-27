cff4854c201a1e5102fc44a003ee5794e79a086d
@pudgebooster <ukraniandollar@gmail.com>
2022-09-27 17:41:28 +0300

    feature: make server implement Connectable

--- server.go
+++ server.go
@@ -3,3 +3,16 @@ package main
 type Server struct {
 	con Connection
 }
+
+func (s *Server) Dial() Connection {
+	recieve := make(chan []byte)
+	send := make(chan []byte)
+	s.con = Connection{
+		Recieve: recieve,
+		Send:    send,
+	}
+	return Connection{
+		Recieve: send,
+		Send:    recieve,
+	}
+}
