3fba21cebf28a1308787609c0764ec864cbe1314
@pudgebooster <ukraniandollar@gmail.com>
2022-09-27 18:04:28 +0300

    feature: add Work to Client

--- client.go
+++ client.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/asstronom/rsa/rsa"
+	"go.mongodb.org/mongo-driver/bson"
+)
 
 type Client struct {
 	con Connection
@@ -15,3 +20,17 @@ func (client *Client) ConnectTo(c Connectable) {
 	client.con = c.Dial()
 }
 
+func (client *Client) Work() error {
+	if client.con.Validate() != nil {
+		return client.con.Validate()
+	}
+	client.con.Send <- []byte{}
+	bytes := <-client.con.Recieve
+	public := rsa.PublicKey{}
+	err := bson.Unmarshal(bytes, &public)
+	if err != nil {
+		return fmt.Errorf("error unmarshaling public key: %s", err)
+	}
+	fmt.Println("client: unmarshaled public key")
+	return nil
+}
