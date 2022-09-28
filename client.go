016f6faf45a95f5094623b95f8c6e699dac14ee9
@pudgebooster <ukraniandollar@gmail.com>
2022-09-28 14:25:15 +0300

    feature: make client send digital signature

--- client.go
+++ client.go
@@ -34,13 +34,44 @@ func (client *Client) Work() error {
 		return fmt.Errorf("error unmarshaling public key: %s", err)
 	}
 	fmt.Println("client: unmarshaled public key")
-	symetricKey := permutation.GenKey(keyLen)
-	fmt.Printf("client: generated symetric key: %v\n", symetricKey)
 	hash := make([]byte, 0)
 	hasher := sha1.New()
 	hasher.Write([]byte(message))
 	hash = hasher.Sum(hash)
 	fmt.Printf("client: hashed message\nhash: %v\n", hash)
-
+	symetricKey := permutation.GenKey(keyLen)
+	fmt.Printf("client: generated symetric key: %v\n", symetricKey)
+	symetricCipher := permutation.NewPermutationCipher(symetricKey)
+	encryptedHash, err := symetricCipher.Encrypt(hash)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("client: encrypted hash with symetric key\nencrypted hash: %v\n", encryptedHash)
+	symetricKeyBytes := []byte{}
+	for i := range symetricKey {
+		symetricKeyBytes = append(symetricKeyBytes, byte(symetricKey[i]))
+	}
+	//fmt.Printf("symetricKeyBytes (len: %d): %v\n", len(symetricKeyBytes), symetricKeyBytes)
+	encryptedSymetricKey := public.Encrypt(symetricKeyBytes)
+	//fmt.Printf("encrypted symetricKeyBytes (len: %d): %v\n", len(encryptedSymetricKey), encryptedSymetricKey)
+	fmt.Printf("client: encrypted symetric key with public key\n")
+	ds := DigitalSignature{
+		SymetricKey: encryptedSymetricKey,
+		MessageHash: encryptedHash,
+	}
+	if isComprimised {
+		ds.Message = []byte(comprimisedMessage)
+	} else {
+		ds.Message = []byte(message)
+	}
+	bytes, err = bson.Marshal(ds)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("client: sent digital signature\n")
+	client.con.Send <- bytes
+	if len(<-client.con.Recieve) != 0 {
+		return fmt.Errorf("server didn't confirm my message :(")
+	}
 	return nil
 }
